e921595b19836936cff597dbf5298a929eac18ee
Divya Deshmukh <divyad1607@gmail.com>
2025-11-27 16:08:32 +0530

    Add

--- src/main/java/com/example/Library_management_system/Controllers/StudentController.java
+++ src/main/java/com/example/Library_management_system/Controllers/StudentController.java
@@ -1,6 +1,8 @@
 package com.example.Library_management_system.Controllers;
 
 import com.example.Library_management_system.Entities.Student;
+import com.example.Library_management_system.Services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 
-public class StudentController {
+public class  StudentController {
+
+    @Autowired
+    private StudentService studentService;
 
     @PostMapping("/add")
     public String addStudent(@RequestBody Student student){
 
+        String result = studentService.addStudent(student);
+        return result;
     }
 }
--- src/main/java/com/example/Library_management_system/Services/StudentService.java
+++ src/main/java/com/example/Library_management_system/Services/StudentService.java
@@ -2,14 +2,20 @@ package com.example.Library_management_system.Services;
 
 
 import com.example.Library_management_system.Entities.Student;
+import com.example.Library_management_system.Repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
     public String addStudent(Student student){
 
-
+        studentRepository.save(student);
+        return "Student has been saved to the DB";
 
     }
 }
